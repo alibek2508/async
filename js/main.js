76ca1ff502e8e5d3e5dfc71dc3a668699d754068
Alibek Tukhbayev <a.tukhbayev@yandex.ru>
2024-02-23 00:40:28 +0600

    fix and add spiner

--- js/main.js
+++ js/main.js
@@ -3,20 +3,27 @@ const img = document.querySelector(".img");
 const author = document.querySelector(".author");
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", handleReload);
-function handleReload() {
-	window.location.reload();
-}
+btn.addEventListener("click", imgGenerator);
 
 async function imgGenerator() {
+	loader.style.display = "block";
+	titleImg.style.display = "none";
+	img.style.display = "none";
+	author.style.display = "none";
+
 	try {
 		let response = await fetch(`https://meme-api.com/gimme/wholesomememes`);
 		let content = await response.json();
 		titleImg.textContent = content.title;
 		img.setAttribute("src", content.url);
 		author.textContent = content.author;
+		img.onload = () => (loader.style.display = "none");
+		titleImg.style.display = "block";
+		img.style.display = "block";
+		author.style.display = "block";
 	} catch (err) {
 		console.error(err);
+		loader.style.display = "none";
 	}
 }
 imgGenerator();
